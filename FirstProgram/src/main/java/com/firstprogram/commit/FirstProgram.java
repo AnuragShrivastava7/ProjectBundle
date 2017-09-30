d5d6bc3ef76711976250b196567f42039e837f8b
AnuragShrivastava7 <31766593+AnuragShrivastava7@users.noreply.github.com>
2017-10-01 00:11:28 +0530

    Update FirstProgram.java

--- FirstProgram/src/main/java/com/firstprogram/commit/FirstProgram.java
+++ FirstProgram/src/main/java/com/firstprogram/commit/FirstProgram.java
@@ -8,7 +8,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		System.out.println("First Program .... ");
 		String[] names = {"Anu", "JHON"};
-		System.out.println(names[1]);
+		System.out.println(names[0]);
+		System.out.println(names[1]);	
 	}
-
 }
